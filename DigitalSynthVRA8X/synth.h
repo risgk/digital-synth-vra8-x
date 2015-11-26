4ea783cff0d502552c387de1680ac49889ae1496
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-11-26 19:58:44 +0900

    Change initial values

--- DigitalSynthVRA8X/synth.h
+++ DigitalSynthVRA8X/synth.h
@@ -27,11 +27,11 @@ public:
     ISynthCore<0>::control_change(OSC_MODE      , 0  );
     ISynthCore<0>::control_change(OSC_COLOR     , 0  );
     ISynthCore<0>::control_change(MOD_RATE      , 8  );
-    ISynthCore<0>::control_change(MOD_DEPTH     , 96 );
+    ISynthCore<0>::control_change(MOD_DEPTH     , 64 );
     ISynthCore<0>::control_change(LPF_CUTOFF_ENV, 64 );
-    ISynthCore<0>::control_change(LPF_RESONANCE , 127);
-    ISynthCore<0>::control_change(ENV_A         , 24 );
-    ISynthCore<0>::control_change(ENV_D_R       , 48 );
+    ISynthCore<0>::control_change(LPF_RESONANCE , 64 );
+    ISynthCore<0>::control_change(ENV_A         , 32 );
+    ISynthCore<0>::control_change(ENV_D_R       , 80 );
   }
 
   INLINE static void receive_midi_byte(uint8_t b) {
