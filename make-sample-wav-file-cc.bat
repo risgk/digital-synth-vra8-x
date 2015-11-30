a0f2108aad458c2500887b76b12ac9ffb562e931
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-11-30 20:00:52 +0900

    Remove an instruction of a JRuby (Ruby) script execution

--- make-sample-wav-file-cc.bat
+++ make-sample-wav-file-cc.bat
@@ -1,4 +1,3 @@
-jruby .\generate-sample-midi-stream.rb
 g++ .\make-sample-wav-file.cc -o .\make-sample-wav-file.exe
 .\make-sample-wav-file.exe .\sample-midi-stream.bin .\sample-wav-file-cc.wav
 del .\make-sample-wav-file.exe
